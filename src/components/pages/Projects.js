336d25d9b11eaf7d70572a695206f6e39c8e6c4f
John Yates <johndyates@gmail.com>
2022-06-15 10:11:59 -0500

    added karmatic

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -6,6 +6,20 @@ export default function Projects() {
     <div className="container">
 
       <div className="row">
+        <div className="col-12">
+          <h4 className="text-center mt-5 font-weight-bold">Project Karmatic:</h4>
+          <br></br>
+          <h5 className="text-left">My favorite project by far. We had 1 week to conceptualize, build and deploy this; and it was the most complex task I had undertaken up to this point. We used React and styled components to structure and build a single page app, while using hooks to display any updates in real time without page refreshes. I started the project working on the front end with one other teammate. We had to plan, communicate and work cohesively so that there wasn't any retroactive changes. I couldn't be more proud of how well we accomplished this task.</h5>
+          <br></br>
+          <h5 className="text-left">After a couple of days the front end was developed enough that I could move on to working on queries and mutations to the database. We used GraphQL and while we did go over it in class, there was a TON of learning on the fly. Hooks and state were another big hurdle we discovered after we got the front and back end talking to eachother. One I wish we had known about before we got as far as we did. But myself and two of the backend devs tackled it as a team. Our dispersment of tasks and communication day to day is something I can truely say was the reason for our success on this project.</h5>
+          <h5 className="text-left">I can proudly claim this as my capstone project.</h5>
+        </div>
+        <div className="col-12">
+          <a target="_blank" href="https://serene-tor-70368.herokuapp.com/">
+            <img src="https://i.imgur.com/OBPLIgn.png" class="mt-5 d-block w-100 mx-auto" alt="..." />
+          </a>
+          <br></br>
+        </div>
         <div className="col-12 col-sm">
           <h4 className="text-center mt-5 font-weight-bold">Project Quacker:</h4>
           <br></br>
@@ -32,7 +46,7 @@ export default function Projects() {
         </div>
         <div className="col-sm-6 col-12">
           <a target="_blank" href="https://cycoconutz.github.io/Project-1/">
-            <img src="https://i.imgur.com/rCoY46S.png" class="mt-5 d-block img-thumbnail mx-auto" alt="..." />
+            <img src="https://i.imgur.com/Nrgf6i5.png" class="mt-5 d-block img-thumbnail mx-auto" alt="..." />
           </a>
         </div>
         <div className="col-12 col-sm">
