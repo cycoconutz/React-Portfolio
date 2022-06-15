1b4b1105d44cb3f65762d60be586dc19a0851271
danny <johndyates@gmail.com>
2022-06-15 07:32:26 -0500

    updated resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Resume() {
   return (
     <div className="pt-3 text-center">
-      <a className="" href="%PUBLIC_URL%/assets/Resume.pdf" download>
-        <img src="https://i.imgur.com/2uzL8Ke.jpg" className='img-fluid img-thumbnail'></img></a>
+      <a className="" download="Danny-Yates-Resume" href="/assets/images/Danny-Yates-Resume.pdf">
+       <img src="https://i.imgur.com/wNdLLeJ.png" title="source: imgur.com" className='img-fluid img-thumbnail'></img></a>
     </div>
   );
 }
