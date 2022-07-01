ce7eb6ddaf9baf6707c74279e0d9ad0c1f109b99
John Yates <johndyates@gmail.com>
2022-07-01 09:14:26 -0500

    updated resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Resume() {
   return (
     <div className="pt-3 text-center">
-      <a className="" download="Danny-Yates-Resume" href="/assets/images/Danny-Yates-Resume.pdf">
-       <img src="https://i.imgur.com/wNdLLeJ.png" title="source: imgur.com" className='img-fluid img-thumbnail'></img></a>
+      <a className="" download="Danny-Yates-Resume" href="/assets/images/DannyYates_PDF.pdf">
+        <img src="https://i.imgur.com/wNdLLeJ.png" title="source: imgur.com" className='img-fluid img-thumbnail'></img></a>
     </div>
   );
 }
